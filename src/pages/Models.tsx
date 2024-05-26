470fd53a4fa341f49e90ff7fb0cf7d57f4e0a6a6
MarcLlopart <markllopart@gmail.com>
2024-05-26 20:40:24 +0200

    model adjustments

--- src/pages/Models.tsx
+++ src/pages/Models.tsx
@@ -1,60 +1,64 @@
+import { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import Web3 from "web3";
+import ModelFactory from "../abi/ModelFactory.json";
+
+interface ModelSource {
+  name: string;
+  hash: string;
+  labels: Array<string>;
+  price: number;
+  accuracy: number;
+}
 
 export default function Models() {
-  const data = [
-    {
-      name: "Cat Detection",
-      labels: ["Cat", "Detection"],
-      hash: "80c1daf4a9a058ae9b8b716c4cdf6dc2eda9ff571f9c25cb7f528a3bb39f48ca",
-      price: 1,
-      accuracy: 60,
-      datasets: [
-        "e5c5ff1408f9793b623b1a9c83a24d27533f05ea7ab93ee67f63f4145dbdb330",
-        "2cc01342ecbde92646b4d1aa6018b5d82f00ea8b743bc6078396c5eed407a0ae",
-      ],
-    },
-    {
-      name: "Cat Detection v2",
-      labels: ["Cat", "Detection"],
-      hash: "b9529d3ad671dd120d4f0e482ca778040808b638c52be73dc39a0b965d190fd1",
-      price: 2,
-      accuracy: 80,
-      datasets: [
-        "e5c5ff1408f9793b623b1a9c83a24d27533f05ea7ab93ee67f63f4145dbdb330",
-        "2cc01342ecbde92646b4d1aa6018b5d82f00ea8b743bc6078396c5eed407a0ae",
-      ],
-    },
-    {
-      name: "Dog Detection",
-      labels: ["Dog", "Detection"],
-      hash: "9ef339f0ad2e21e9827990e73882224e2de8ed97b1d95d387f9e67ba04f50a68",
-      price: 0.5,
-      accuracy: 90,
-      datasets: ["95ca0e969408d1a8987780da9657764d84e36b093bb2f9ead00668c3fd7ebef9"],
-    },
-  ];
+  const [data, setData] = useState<ModelSource[]>([]);
+
+  useEffect(() => {
+    async function getModelSources() {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        const modelSource = new web3.eth.Contract(ModelFactory, "0x8185E0DbAF89f2384E7502FF5a87104974d7A947");
+
+        const modelSources = await modelSource.methods
+          .getModelSources(1)
+          .call()
+          .catch((error) => console.error(error));
+
+        console.log(modelSources)
+
+        if (modelSource) {
+          setData(modelSources as []);
+        }
+      }
+    }
+    getModelSources();
+  }, []);
+
   return (
     <div className="flex flex-col w-full">
       <div className="flex justify-between border-b-[0.5px] border-neutral-700 py-2">
-        <span className="text-2xl font-semibold">Trained Models</span>
-        <Link className="flex justify-center items-center" to="/new-model">
+        <span className="text-2xl font-semibold">Model Sources</span>
+        <Link className="flex justify-center items-center" to="/new-dataset">
           <button
             type="button"
-            className="flex items-center text-amber-300 border-2 border-amber-300 hover:bg-amber-300 hover:bg-opacity-30 p-2 rounded-lg"
+            className="flex items-center text-amber-300 border-2 border-amber-300 hover:bg-amber-300 hover:bg-opacity-30  p-2 rounded-lg"
           >
             <FaPlus className="w-5" />
           </button>
         </Link>
       </div>
-      <div className="flex flex-col w-full overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="text-sm text-left rtl:text-righ my-6">
+      <div className="flex flex-col w-full overflow-x-auto shadow-md sm:rounded-lg my-6">
+        <table className="text-sm text-left rtl:text-righ">
           <thead className="text-xs  uppercase bg-[#ffffff0d] text-gray-400">
             <tr>
               <th className="px-6 py-3">Name</th>
               <th className="px-6 py-3">Labels</th>
               <th className="px-6 py-3">Hash</th>
-              <th className="px-6 py-3">Datasets</th>
               <th className="px-6 py-3">Accuracy</th>
               <th className="px-6 py-3">Price</th>
             </tr>
@@ -66,9 +70,8 @@ export default function Models() {
                 name={value.name}
                 labels={value.labels}
                 hash={value.hash}
-                price={value.price}
-                accuracy={value.accuracy}
-                datasets={value.datasets}
+                price={Number(value.price)}
+                accuracy={Number(value.accuracy)}
               />
             ))}
           </tbody>
@@ -83,11 +86,10 @@ interface ModelProps {
   labels: Array<string>;
   hash: string;
   price: number;
-  datasets: Array<string>;
   accuracy: number;
 }
 
-const ModelRow = ({ name, labels, hash, price, datasets, accuracy }: ModelProps) => (
+const ModelRow = ({ name, labels, hash, price, accuracy}: ModelProps) => (
   <tr className="bg-[#ffffff05] border-gray-700 text-white hover:!text-amber-300">
     <td className="px-6 py-4 font-medium whitespace-nowra">{name}</td>
     <td className="px-6 py-4">
@@ -99,12 +101,7 @@ const ModelRow = ({ name, labels, hash, price, datasets, accuracy }: ModelProps)
       ))}
     </td>
     <td className="px-6 py-4">{hash}</td>
-    <td className="flex flex-col px-6 py-4">
-      {datasets.map((value) => (
-        <span key={value}>{value}</span>
-      ))}
-    </td>
-    <td className="px-6 py-4">{accuracy}%</td>
     <td className="px-6 py-4">{price} DEV</td>
+    <td className="px-6 py-4">{accuracy} %</td>
   </tr>
 );
