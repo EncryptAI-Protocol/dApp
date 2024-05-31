e5536f36d213303286391e64ef804dfaef9be576
MarcLlopart <markllopart@gmail.com>
2024-05-31 20:29:07 +0200

    update wallet button

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { MetaMaskProvider } from "@metamask/sdk-react";
+import '@rainbow-me/rainbowkit/styles.css';
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+import Wallet from "./components/Wallet/Wallet.tsx";
+import { config } from './wagmi';
+
 import CreateDataset from "./pages/CreateDataset.tsx";
 import CreateModel from "./pages/CreateModel.tsx";
 import Datasets from "./pages/Datasets.tsx";
 import Inference from "./pages/Inference.tsx";
 import Models from "./pages/Models.tsx";
 import Root from "./pages/Root.tsx";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
 const router = createBrowserRouter([
   {
@@ -39,18 +50,17 @@ const router = createBrowserRouter([
   },
 ]);
 
-// biome-ignore lint: Ignore noNullAssertion
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+root.render(
   <React.StrictMode>
-    <MetaMaskProvider
-      debug={false}
-      sdkOptions={{
-        dappMetadata: {
-          url: window.location.href,
-        },
-      }}
-    >
-      <RouterProvider router={router} />
-    </MetaMaskProvider>
-  </React.StrictMode>,
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+        <Wallet/>
+        <RouterProvider router={router} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
 );
